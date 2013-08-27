e9fffcaff8fb58ffa6d25ba8e635bef9f492987d
Gabriel Nadler <gabriel.nadler@swisscom.com>
2013-08-27 11:24:42 +0200

    Implemented term functions seem to work. toString() returns a string-grid of the written stuff

--- term.lua
+++ term.lua
@@ -1,6 +1,6 @@
 local color = {
-  ["white"] = 1
-  ["black"] = 2
+  ["white"] = 1,
+  ["black"] = 2,
 }
 
 term = {}
@@ -12,10 +12,18 @@ term.textColor = color.white
 term.backgroundColor = color.black
 term.cursorBlink = true
 
+term.emptyCell = {
+  ["content"] =  " ",
+  ["background"] =  color.black,
+  ["contentColor"] =  color.white,
+}
+
 function term.initGrid()
-  for x = 1, term.size[1], 1 do
-    for y = 1, term.size[2], 1 do
-      term.grid[x][y] = ""
+  local width, height = term.getSize()
+  for x = 1, width, 1 do
+    term.grid[x] = {}
+    for y = 1, height, 1 do
+      term.grid[x][y] = term.emptyCell
     end
   end
 end
@@ -30,24 +38,37 @@ function term.configure(configuration)
 end
 
 function term.setTextColor(color)
-  if (color == color.white or color = color.black) then
+  if (color == color.white or color == color.black) then
     term.textColor = color
   end
 end
 
 function term.setBackgroundColor(color)
-    if (color == color.white or color = color.black) then
+    if (color == color.white or color == color.black) then
     term.backgroundColor = color
   end
 end
 
 function term.write(text)
   local x,y = term.getCursorPos()
-  for i=0,#text-1 do
+  local width, height = term.getSize()
+  local currentCol, currentLine = x,y
+  local colOffset = 0
+  for i=1,#text do
     --TODO: Wrapping of lines
-    local xpos = x+i-1 -- Take one off because we want to start writing in the current Position
-    term.grid[xpos][y] = string.sub(text, i,i)
+    currentCol = currentCol + 1
+    local letter = string.sub(text,i,i)
+    if currentCol >width then
+      currentCol = 1
+      currentLine = currentLine + 1
+    end
+     term.grid[currentLine][currentCol] = {
+        ["content"] = letter,
+        ["background"] = term.backgroundColor,
+        ["textColor"] = term.textColor,
+      }
   end
+  term.setCursorPos(currentCol, currentLine)
   return text
 end
 
@@ -67,7 +88,7 @@ end
 function term.clearLine()
   local x,y = term.getSize()
   for x=1, term.size[2], 1 do
-    term.grid[x][y] = ""
+    term.grid[x][y] = term.emptyCell
   end
 end
 function term.setCursorBlink(state)
@@ -92,5 +113,32 @@ end
 function term.restore()
   -- not implemented
 end
+
+
+function term.toString()
+  local str = ""
+  local width, height = term.getSize()
+  for x=1, width, 1 do
+    for y=1, height, 1 do
+      str = str .. term.grid[x][y].content
+    end
+    str = str .. "\n"
+  end
+  return str
+end
+
+-- term.configure({["size"] = {5,6}})
+term.initGrid()
+term.write("hey")
+print(term.toString())
+print("--------------------------")
+local x,y = term.getCursorPos()
+term.setCursorPos(x-1, y)
+term.write(" ")
+term.setCursorPos(x-1, y)
+term.write("haha")
+print(term.toString())
+-- term.grid[1][1] = "x"
+-- term.grid[1][4] = "x"
 
 
